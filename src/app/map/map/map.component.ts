63dac6d60e03b919b9f106a8b99858caca695119
MeerrettichMeister <froesenico@gmail.com>
2024-01-27 13:44:05 +0100

    Fix broken markers

--- src/app/map/map/map.component.ts
+++ src/app/map/map/map.component.ts
@@ -3,7 +3,21 @@ import * as L from 'leaflet';
 import {layerGroup} from "leaflet";
 import {MatIcon} from "@angular/material/icon";
 import {filter} from "rxjs";
-
+import { icon, Marker } from 'leaflet';
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+Marker.prototype.options.icon = iconDefault;
 @Component({
   selector: 'app-map',
   standalone: true,
